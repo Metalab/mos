9479de9caf896a62926688c21b9de228e7d65481
Christoph Schindler <hop@30hopsmax.at>
2012-04-21 02:56:06 +0200

    Replace deprecated template loaders.

--- settings.py
+++ settings.py
@@ -59,8 +59,8 @@ ADMIN_MEDIA_PREFIX = '/admin_media/'
 
 # List of callables that know how to import templates from various sources.
 TEMPLATE_LOADERS = (
-    'django.template.loaders.filesystem.load_template_source',
-    'django.template.loaders.app_directories.load_template_source',
+    'django.template.loaders.filesystem.Loader',
+    'django.template.loaders.app_directories.Loader', 
 )
 
 MIDDLEWARE_CLASSES = (
