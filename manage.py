b23511d591996b83b65c9d19ec7a71ab5c09c2f1
Christoph Schindler <hop@30hopsmax.at>
2012-06-25 01:42:38 +0200

    Update manage.py to version from Django 1.4.

--- manage.py
+++ manage.py
@@ -1,22 +1,10 @@
 #!/usr/bin/env python
-import sys
 import os
-
-_DIRNAME = os.path.dirname(globals()["__file__"])
-sys.path.append(os.path.join(_DIRNAME, '..'))
-sys.path.append('/django/svn/trunk')
-from django.core.management import execute_manager
-try:
-    import settings # Assumed to be in the same directory.
-except ImportError:
-    import sys
-    sys.stderr.write("Error: Can't find the file 'settings.py' in the \
-                      directory containing %r. It appears you've \
-                      customized things.\nYou'll have to run django-admin.py,\
-                      passing it your settings module.\n(If the file \
-                      settings.py does indeed exist, it's causing an \
-                      ImportError somehow.)\n" % __file__)
-    sys.exit(1)
+import sys
 
 if __name__ == "__main__":
-    execute_manager(settings)
+    os.environ.setdefault("DJANGO_SETTINGS_MODULE", "mos.settings.devel")
+
+    from django.core.management import execute_from_command_line
+
+    execute_from_command_line(sys.argv)
