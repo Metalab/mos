9f646d9b016cf94bde9b678f68d88ed6d79ad51c
Thomas Perl <m@thp.io>
2012-09-20 21:58:39 +0200

    Import fix: Add '..' to sys.path

--- manage.py
+++ manage.py
@@ -3,6 +3,13 @@ import os
 import sys
 
 if __name__ == "__main__":
+    # Dirty hack. Right now, we need this in order for the settings module
+    # "mos.settings.devel" to be found (plus many other things that import
+    # "mos and its child modules/packages). Ideally we should create a new
+    # package "mos" in this directory and move all files there, and then
+    # remove this line.
+    sys.path.insert(0, '..')
+
     os.environ.setdefault("DJANGO_SETTINGS_MODULE", "mos.settings.devel")
 
     from django.core.management import execute_from_command_line
