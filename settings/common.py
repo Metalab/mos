6fd62267232693092b4ea3b40a98fc2f67d71318
Markus Prinz <markus.prinz@nuclearsquid.com>
2013-07-12 19:27:44 +0200

    Add require_debug_false filter as per Django deprecation warning

--- settings/common.py
+++ settings/common.py
@@ -170,6 +170,9 @@ LOGGING = {
         },
     },
     'filters': {
+        'require_debug_false': {
+            '()': 'django.utils.log.RequireDebugFalse',
+        }
     },
     'handlers': {
         'null': {
@@ -189,6 +192,7 @@ LOGGING = {
         },
         'mail_admins': {
             'level': 'ERROR',
+            'filters': ['require_debug_false'],
             'class': 'django.utils.log.AdminEmailHandler',
         },
     },
