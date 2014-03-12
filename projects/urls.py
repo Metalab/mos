7116ea7bb2baf3e7da25f397d2bb6e3a1c664226
Christoph Schindler <hop@30hopsmax.at>
2014-03-12 22:08:59 +0100

    Fix detail view for projects.
    The template was missing (so it actually didn't work on the live site
    either) and the generic view class was used the wrong way. The new
    template is copied over from the calendar module.

--- projects/urls.py
+++ projects/urls.py
@@ -1,26 +1,19 @@
 from django.conf.urls import *
-from django.views.generic import ListView
+from django.views.generic import ListView, DetailView
 
 from mos.projects.models import Project
 
-info_dict = {
-    'queryset': Project.all.all(),
-}
 
-
-urlpatterns = patterns('django.views.generic.dates',
-        (r'^$', 
-         ListView.as_view(queryset=Project.all.all().order_by('-created_at')[:5],
-                          context_object_name="latestprojects",
-                          template_name="projects/project_archive.html")),
-)
-
-urlpatterns += patterns('',
+urlpatterns = patterns('',
+        (r'^$', ListView.as_view(
+            queryset=Project.all.all().order_by('-created_at')[:5],
+            context_object_name="latestprojects",
+            template_name="projects/project_archive.html")),
         (r'^(?P<object_id>\d+)/delete/$',
             'mos.projects.views.delete_project'),
-        (r'^(?P<object_id>\d+)/$',
-            'django.views.generic.detail.DetailView',
-            info_dict),
+        (r'^(?P<pk>\d+)/$', DetailView.as_view(
+            queryset=Project.all.all()
+        )),
         (r'^(?P<object_id>\d+)/update/$',
             'mos.projects.views.update_project', {'new': False}),
         (r'^new/$',
