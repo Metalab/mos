8618b3fdced1a4d5b4b38621c52c95018a74ecbd
Christoph Schindler <hop@30hopsmax.at>
2014-03-12 23:07:39 +0100

    Fix archive view of 'rss' app.
    Which is to say, the list of recent wiki changes. This does appear
    not to be used anywhere, but there you go...

--- rss/urls.py
+++ rss/urls.py
@@ -1,18 +1,10 @@
 from django.conf.urls import *
-from mos import settings
+from django.views.generic import ArchiveIndexView
 
 from models import Change
 
-date_dict = {
-    'queryset': Change.objects.all(),
-    'date_field': 'updated',
-    'allow_future': True,
-    'allow_empty': True,
-    'num_latest': 5,
-    'template_object_name': 'latestchanges',
-}
 
-urlpatterns = patterns('django.views.generic.dates',
+urlpatterns = patterns('',
 #(r'^(?P<year>\d{4})/(?P<month>[a-z]{3})/(?P<day>\w{1,2})/(?P<slug>[-\w]+)/$',
 # 'object_detail', date_dict),
 #(r'^(?P<year>\d{4})/(?P<month>[a-z]{3})/(?P<day>\w{1,2})/$',
@@ -20,5 +12,11 @@ urlpatterns = patterns('django.views.generic.dates',
 #(r'^(?P<year>\d{4})/(?P<month>[a-z]{3})/$',
 #'archive_month', date_dict),
 #(r'^(?P<year>\d{4})/$', archive_year',  date_dict),
-    (r'^$', 'ArchiveIndexView', date_dict),
+    (r'^$', ArchiveIndexView.as_view(
+        queryset = Change.objects.all(),
+        date_field = 'updated',
+        allow_future = True,
+        allow_empty = True,
+        context_object_name='latestchanges',
+    )),
 )
