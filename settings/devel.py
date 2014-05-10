Metalab/mos

commit 5ae37fe91d9bd498f1443c3fee17b58dbe11ed27

Fixed user-uploaded media when using runserver.
Updated devel settings as well.

settings/devel.py
@@ -14,3 +14,8 @@ DATABASES = {
         'PORT': '',
     }
 }
+
+STATIC_ROOT = PROJECT_DIR.child("static")
+MEDIA_ROOT = PROJECT_DIR.child("media")
+
+HOS_SEPA_CREDITOR_ID = 'AT29HXR00000037632'

urls.py
@@ -1,6 +1,7 @@
 from django.conf import settings
 
 from django.conf.urls import *
+from django.conf.urls.static import static
 from django.contrib import admin
 
 from mos.cal.feeds import EventFeed
@@ -30,3 +31,5 @@ urlpatterns = patterns('',
 
     (r'^cellardoor/', 'mos.web.views.display_cellardoor'),
 )
+
+urlpatterns +=  static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)

commit fd83b56f9e1b03f94ca67e06c2604d14d1a59b99

Fixed regression in 5ae37fe.

settings/devel.py
@@ -15,7 +15,6 @@ DATABASES = {
     }
 }
 
-STATIC_ROOT = PROJECT_DIR.child("static")
 MEDIA_ROOT = PROJECT_DIR.child("media")
 
 HOS_SEPA_CREDITOR_ID = 'AT29HXR00000037632'