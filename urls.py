b7e23c3ec5d387209cf24a57f99aa451dec4b1e0
Christoph Schindler <hop@30hopsmax.at>
2014-05-11 23:55:04 +0200

    Fixed fancy date time picker for non-admins.

--- urls.py
+++ urls.py
@@ -10,22 +10,18 @@ from mos.cal.feeds import EventFeed
 admin.autodiscover()
 
 urlpatterns = patterns('',
+    (r'^admin/jsi18n/', 'django.views.i18n.javascript_catalog'),
     (r'^admin/', include(admin.site.urls)),
 
     (r'^feeds/events/$', EventFeed()),
-
-    (r'^calendar/', include('mos.cal.urls')),
     (r'^rss/', include('mos.rss.urls')),
 
+    (r'^calendar/', include('mos.cal.urls')),
     (r'^project/', include('mos.projects.urls')),
-
-    (r'^$', 'mos.web.views.display_main_page'),
-
     (r'^member/', include('mos.members.urls')),
-
     (r'^announce/$', include('mos.announce.urls')),
-
     (r'^cellardoor/', 'mos.web.views.display_cellardoor'),
+    (r'^$', 'mos.web.views.display_main_page'),
 )
 
 urlpatterns +=  static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
