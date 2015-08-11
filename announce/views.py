0383462601e823c33f796b4bc6c530e2a8620f9b
Florian Schweikert <kelvan@ist-total.org>
2015-08-12 01:21:36 +0200

    Clean announce view

--- announce/views.py
+++ announce/views.py
@@ -2,12 +2,11 @@
 # Views for issueing announcements to all active members.
 #
 import smtplib
-from datetime import *
+from datetime import date
 
-from django.shortcuts import render_to_response
-from django.template import RequestContext
+from django.shortcuts import render
 from django.conf import settings
-from django.contrib.auth.decorators import user_passes_test
+from django.contrib.admin.views.decorators import staff_member_required
 import django.forms as forms
 from django.core.mail import send_mail
 
@@ -16,25 +15,21 @@ from members.models import get_active_members, ContactInfo
 
 class AnnouncementForm(forms.Form):
     subject = forms.CharField(required=True, label="Thema", max_length=40)
-    body = forms.CharField(required=True, label="Mitteilung",widget=forms.Textarea,)
-    to = forms.ChoiceField(required=True, label="An", choices=(('collection','collection'), ('all','all'),))
+    body = forms.CharField(required=True, label="Mitteilung",
+                           widget=forms.Textarea)
+    to = forms.ChoiceField(required=True, label="An",
+                           choices=(('collection', 'collection'), ('all', 'all'),))
 
 
-@user_passes_test(lambda u: (u.is_staff and u.is_authenticated()))
+@staff_member_required
 def announce(request):
-    print 'wtf:' + repr(request.user)
     form = AnnouncementForm(request.POST or None)
     if not request.POST or not form.is_valid():
-        return render_to_response('announce/write_message.html',
-                                  {
-                                   'form': form,
-                                   'user': request.user,
-                                  }, context_instance=RequestContext(request))
+        context = {'form': form, 'user': request.user}
+        return render(request, 'announce/write_message.html', context)
     # Valid message: send it!
-    s = ''
-
     users = get_active_members()
-    if form.cleaned_data['to']!='all':
+    if form.cleaned_data['to'] != 'all':
         users = users.filter(paymentinfo__bank_collection_allowed=True)\
                      .filter(paymentinfo__bank_collection_mode__id=4)
         for u in users:
@@ -52,17 +47,13 @@ def announce(request):
                       fail_silently=False)
             ci.last_email_ok = True
             ci.save()
-        except smtplib.SMTPException, instance:
+        except smtplib.SMTPException as e:
             f = open(settings.HOS_ANNOUNCE_LOG, 'a')
             f.write('\n\n'+user.email)
-            f.write('\n'+repr(instance))
+            f.write('\n'+repr(e))
             ci.last_email_ok = False
             ci.save()
             f.close()
 
-    return render_to_response('announce/message_sent.html',
-                              {
-                               'form': form,
-                               'user': request.user,
-                               'users': users,
-                               }, context_instance=RequestContext(request))
+    context = {'form': form, 'user': request.user, 'users': users}
+    return render(request, 'announce/message_sent.html', context)
