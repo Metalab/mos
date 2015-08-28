dc7e4aee0441f048a990ab53f9940de553e93f75
Florian Schweikert <kelvan@ist-total.org>
2015-08-28 20:48:13 +0200

    Fixed dates in EventFactory
    Factory set the method today as start and end date instead of the return value
    of today()
    Caused error when trying to save the created event

--- cal/factories.py
+++ cal/factories.py
@@ -1,14 +1,15 @@
 import datetime
 import factory
 import faker
-from models import Event
+
+from .models import Event
 
 
 class EventFactory(factory.Factory):
     name = faker.company.bs()
     teaser = faker.company.catch_phrase()
-    startDate = datetime.date.today
-    endDate = datetime.date.today
+    startDate = datetime.date.today()
+    endDate = datetime.date.today()
 
     class Meta:
         model = Event
