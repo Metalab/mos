b2f6d158ab7cb4693cf53475c8f3c24139546c2b
norpol <norpol@woerm.at>
2016-02-12 13:59:09 +0100

    Convert data to string before join

--- members/models.py
+++ members/models.py
@@ -428,7 +428,7 @@ class Payment(models.Model):
     objects = PaymentManager()
 
     def __str__(self):
-        return ', '.join([self.date, self.amount, self.user.username, self.method.name])
+        return ', '.join([str(self.date), str(self.amount), str(self.user.username), str(self.method.name)])
 
     class Meta:
         ordering = ['date']
