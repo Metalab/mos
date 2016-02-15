Metalab/mos

commit 20739f1d9a2b3a580d154b507f1ccdb403bf32db

Fix paths, move settings/urls to new module

members/models.py
@@ -1,18 +1,14 @@
-#!/usr/bin/python
-# vim: set fileencoding=utf-8
+#encoding=utf-8
+from __future__ import unicode_literals, print_function
 
-import datetime
+from datetime import datetime, date
 from decimal import Decimal
 
 from django.db import models
+from django.db.models import Q
 from django.conf import settings
 from django.contrib.auth.models import User
-from django.db.models import Q
-from django.core.exceptions import ObjectDoesNotExist
-from django.contrib.admin import StackedInline, TabularInline
-from django.contrib.auth.admin import UserAdmin
-from django.utils.encoding import smart_unicode
-
+from django.utils.encoding import python_2_unicode_compatible, smart_unicode
 
 
 class PaymentInfo(models.Model):
@@ -38,7 +34,7 @@ class ContactInfo(models.Model):
 
     def get_image_path(self, filename):
         name, ext = filename.rsplit('.', 1)
-        return 'userpics/%s.%s' %(self.user.username, ext)
+        return 'userpics/%s.%s' % (self.user.username, ext)
 
     on_intern_list = models.BooleanField(default=True)
     intern_list_email = models.EmailField(blank=True)
@@ -65,21 +61,19 @@ class ContactInfo(models.Model):
                                        max_digits=3, decimal_places=2)
     remark = models.TextField(null=True, blank=True)
 
-
     def get_debts(self):
-        #FIXME: this is broken because it assumes that a membership period
+        # FIXME: this is broken because it assumes that a membership period
         #       has a constant fee
         arrears = 0
         mp_list = MembershipPeriod.objects.filter(user=self.user)
         for mp in mp_list:
-            fee = MembershipFee.objects.get(kind_of_membership=\
-                                                mp.kind_of_membership)
+            fee = MembershipFee.objects.get(kind_of_membership=mp.kind_of_membership)
             if fee.amount > 0:
-                arrears += mp.get_duration_in_month()*fee.amount
+                arrears += mp.get_duration_in_month() * fee.amount
         return arrears - self.get_all_payments()
 
     def get_debt_for_month(self, date_in_month):
-        #see if the there is a membership period for the month
+        # see if the there is a membership period for the month
         mp_list = MembershipPeriod.objects.filter(user=self.user).filter(
                     Q(begin__lte=date_in_month),
                     Q(end__isnull=True) | Q(end__gte=date_in_month))
@@ -87,8 +81,8 @@ class ContactInfo(models.Model):
         if mp_list.count() == 0:
             return 0
         else:
-           #find the membership fee for the month and kind
-            #of membership and return amount
+            # find the membership fee for the month and kind
+            # of membership and return amount
             mp = mp_list[0]
             fee = mp.kind_of_membership.membershipfee_set.filter(
                 Q(start__lte=date_in_month),
@@ -128,31 +122,23 @@ class ContactInfo(models.Model):
         #                    sf - 2010 07 27
         mp = MembershipPeriod.objects.filter(user=self.user)\
             .order_by('-begin')[0]
-        if not mp.end is None:
+        if mp.end is not None:
             return False
 
-        return not self.key_id is None and self.has_active_key
+        return self.key_id is not None and self.has_active_key
 
     def get_wikilink(self):
-        wikiname = self.wiki_name
-        if not wikiname:
-            wikiname = self.user.username
+        wikiname = self.wiki_name or self.user.username
 
         return u'%sBenutzer:%s' % (settings.HOS_WIKI_URL, wikiname)
 
 
-def get_active_members():
-    return User.objects.filter(
-                Q(membershipperiod__begin__lte=datetime.datetime.now()),
-                Q(membershipperiod__end__isnull=True) |
-                Q(membershipperiod__end__gte=datetime.datetime.now()))\
-                .distinct()
-
 def get_mailinglist_members():
     return User.objects.filter(
                 Q(membershipperiod__end__isnull=True) |
-                Q(membershipperiod__end__gte=datetime.datetime.now()))\
+                Q(membershipperiod__end__gte=datetime.now()))\
                 .distinct()
+
 
 def get_active_members_for(dt):
     return User.objects.filter(
@@ -161,11 +147,17 @@ def get_active_members_for(dt):
                 Q(membershipperiod__end__gte=dt))\
                 .distinct()
 
+
+def get_active_members():
+    return get_active_members_for(datetime.now())
+
+
 def get_active_and_future_members():
     return User.objects.filter(
                 Q(membershipperiod__end__isnull=True) |
-                Q(membershipperiod__end__gte=datetime.datetime.now()))\
+                Q(membershipperiod__end__gte=datetime.now()))\
                 .distinct()
+
 
 def get_active_membership_months_until(date):
     periods = MembershipPeriod.objects.filter(Q(begin__lte=date))
@@ -175,54 +167,59 @@ def get_active_membership_months_until(date):
         end = get_months(date if p.end is None or p.end > date else p.end)
         nrMonths = end - begin + 1
         kind = p.kind_of_membership.name
-        if res.has_key(kind):
+        if kind in res:
             res[kind] += nrMonths
         else:
             res[kind] = nrMonths
 
     return res
 
-def get_months(date):
-    return date.month + 12*date.year
 
+def get_months(date):
+    return date.month + 12 * date.year
+
+
+@python_2_unicode_compatible
 class BankCollectionMode(models.Model):
     name = models.CharField(max_length=20)
     num_month = models.IntegerField()
 
-    def __unicode__(self):
-        return u"%s" % self.name
+    def __str__(self):
+        return self.name
 
 
+@python_2_unicode_compatible
 class MembershipPeriod(models.Model):
     begin = models.DateField()
     end = models.DateField(null=True, blank=True)
     user = models.ForeignKey(User)
     kind_of_membership = models.ForeignKey('KindOfMembership')
 
-    def __unicode__(self):
-        return u"%s" % self.user.username
+    def __str__(self):
+        return self.user.username
 
     def get_duration_in_month(self):
-        if self.end is None or self.end > datetime.date.today():
-            end = datetime.date.today()
+        if self.end is None or self.end > date.today():
+            end = date.today()
         else:
             end = self.end
         if end < self.begin:
             return 0
 
-        begin = datetime.date(self.begin.year, self.begin.month, 1)
-        end = datetime.date(end.year, end.month, 2)
+        begin = date(self.begin.year, self.begin.month, 1)
+        end = date(end.year, end.month, 2)
 
         month = 0
         while begin < end:
             if begin.month == 12:
-                begin = datetime.date(begin.year + 1, 1, 1)
+                begin = date(begin.year + 1, 1, 1)
             else:
-                begin = datetime.date(begin.year, begin.month + 1, 1)
+                begin = date(begin.year, begin.month + 1, 1)
             month += 1
         return month
 
 
+@python_2_unicode_compatible
 class MembershipFee(models.Model):
     """
     Defines the membership fee for a certain period of time.
@@ -236,15 +233,16 @@ class MembershipFee(models.Model):
     end = models.DateField(null=True, blank=True)
     amount = models.IntegerField()
 
-    def __unicode__(self):
-        return u"%s - %d" % (self.kind_of_membership, self.amount)
+    def __str__(self):
+        return "%s - %d" % (self.kind_of_membership, self.amount)
 
 
+@python_2_unicode_compatible
 class KindOfMembership(models.Model):
     name = models.CharField(max_length=30)
 
-    def __unicode__(self):
-        return u"%s" % self.name
+    def __str__(self):
+        return self.name
 
 
 class PaymentManager(models.Manager):
@@ -255,70 +253,70 @@ class PaymentManager(models.Manager):
         r = csv.reader(f, delimiter=";")
 
         for line in r:
-            if len(line)<2:
-                print line
+            if len(line) < 2:
+                print(line)
                 continue
             try:
                 u = User.objects.get(first_name=smart_unicode(line[0]), last_name=smart_unicode(line[1]))
             except User.DoesNotExist:
-                print line
+                print(line)
                 continue
             except:
-                print "exception on line:"
-                print line
+                print("exception on line:")
+                print(line)
                 raise
 
             sum = line[5]
             try:
                 Payment.objects.create(date=date, user=u, amount=sum, method=PaymentMethod.objects.get(name='bank collection'), original_file=filename, original_line=str(line))
-            except ValueError, e:
-                print line
+            except ValueError:
+                print(line)
 
-    #used to import generic payments, including date and payment type (as opposed to import_smallfile)
+    # used to import generic payments, including date and payment type (as opposed to import_smallfile)
     def import_generic(self, filename):
         import csv
 
         f = open(filename, 'r')
         r = csv.reader(f, delimiter=";")
 
-        nr_imported = 0
-
         for line in r:
-            if len(line)<9:
-                print 'malformed: ' + repr(line)
+            if len(line) < 9:
+                print('malformed:', repr(line))
                 continue
             try:
                 u = User.objects.get(first_name=smart_unicode(line[0]), last_name=smart_unicode(line[1]))
             except User.DoesNotExist:
-                print 'user not found: ' + repr(line)
+                print('user not found:', repr(line))
                 continue
 
             sum, date, method = (line[5], line[7], line[8])
             try:
                 p = Payment.objects.filter(date=date, amount=sum, user=u)
                 if len(p) != 0:
-                   print 'payment already present: ' + repr(line)
-                   continue
+                    print('payment already present:', repr(line))
+                    continue
                 Payment.objects.create(date=date, user=u, amount=sum, method=PaymentMethod.objects.get(name=method), original_file=filename, original_line=str(line))
-                print 'created: ' + repr(line)
-            except ValueError, e:
-                print 'error creating payment: ' + repr(line)
-
+                print('created:', repr(line))
+            except ValueError:
+                print('error creating payment:', repr(line))
 
     def import_hugefile(self, filename):
         import csv
-        from decimal import Decimal
 
         f = open(filename, 'r')
         r = csv.reader(f, delimiter=";")
 
-        i=0
-
+        i = 0
         for line in r:
-            i+=1
+            i += 1
             if not line[0]:
                 continue
-            pms = PaymentMethod.objects.filter(name=line[3] if not line[3] in ('sammler', 'Umlaufvermögen:2810 Bank') else 'bank collection')
+
+            if line[3] in ('sammler', 'Umlaufvermögen:2810 Bank'):
+                pms_name = 'bank collection'
+            else:
+                pms_name = line[3]
+            pms = PaymentMethod.objects.filter(name=pms_name)
             if pms:
                 pm = pms[0]
             else:
@@ -342,8 +340,8 @@ class PaymentManager(models.Manager):
             try:
                 sum = Decimal(sum) / len(list)
             except Exception, e:
-                print e
-                print line
+                print(e)
+                print(line)
                 continue
 
             for name in list:
@@ -355,8 +353,8 @@ class PaymentManager(models.Manager):
                 if fragments[0] == '':
                     fragments = fragments[1:]
 
-                if len(fragments)==0:
-                    print 'aaaaaaaaaahhh!!'
+                if len(fragments) == 0:
+                    print('aaaaaaaaaahhh!!')
                     continue
 
                 if fragments[0] == 'fehlgeschlagen':
@@ -368,8 +366,7 @@ class PaymentManager(models.Manager):
                 if fragments[0] == 'Ewald-Oliver':
                     fragments[0] = 'Oliver'
 
-
-                if len(fragments)>1:
+                if len(fragments) > 1:
                     if fragments[1] in ('Sirek', 'Siereck'):
                         fragments[1] = 'Sierek'
 
@@ -379,35 +376,39 @@ class PaymentManager(models.Manager):
                     if fragments[1] == 'Grenzfurtner':
                         fragments[1] = 'Grenzfurthner'
 
-                    if fragments[1] in ('Berg',): # Berg San, Leo Findeisen
+                    if fragments[1] in ('Berg',):  # Berg San, Leo Findeisen
                         fragments = fragments[1:]
 
                     if fragments[1] in ('Leo',):
                         fragments = [fragments[0], fragments[2]]
 
                     if fragments[0] == 'Schreiner':
-                        fragments = [fragments[1] , fragments[0]]
+                        fragments = [fragments[1], fragments[0]]
 
                     if fragments[1] == 'Manztos':
                         fragments[1] = 'Mantzos'
 
-                    if len(fragments)>2 and fragments[2]:
+                    if len(fragments) > 2 and fragments[2]:
                         if fragments[2] == 'Laub':
                             fragments = fragments[1:]
 
-                if(len(fragments)>1):
+                if(len(fragments) > 1):
                     u = User.objects.filter(first_name__iexact=fragments[0], last_name__iexact=fragments[1])
-                    if len(u)!=1:
-                        print u, fragments
+                    if len(u) != 1:
+                        print(u, fragments)
                     else:
-                        Payment.objects.create(user=u[0], amount=sum, date=line[0], method=pm, original_line = str(line), original_file = filename, original_lineno = i)
-
+                        Payment.objects.create(user=u[0], amount=sum,
+                                               date=line[0], method=pm,
+                                               original_line=str(line),
+                                               original_file=filename,
+                                               original_lineno=i)
 
                 else:
-                    print 'no user found for'
-                    print line
-        pass
+                    print('no user found for')
+                    print(line)
 
+
+@python_2_unicode_compatible
 class Payment(models.Model):
     amount = models.FloatField()
     comment = models.CharField(max_length=200, blank=True)
@@ -420,39 +421,16 @@ class Payment(models.Model):
 
     objects = PaymentManager()
 
-    def __unicode__(self):
-        return u"%s, %s, %s, %s" % (self.date, self.amount, self.user.username, self.method.name)
+    def __str__(self):
+        return ', '.join([self.date, self.amount, self.user.username, self.method.name])
 
     class Meta:
         ordering = ['date']
 
 
+@python_2_unicode_compatible
 class PaymentMethod(models.Model):
     name = models.CharField(max_length=50)
 
-    def __unicode__(self):
-        return u"%s" % self.name
-
-
-class ContactInfoInline(StackedInline):
-    model = ContactInfo
-    max_num = 1
-
-class PaymentInfoInline(StackedInline):
-    model = PaymentInfo
-    max_num = 1
-
-class MembershipPeriodInline(TabularInline):
-    model = MembershipPeriod
-
-class PaymentInline(TabularInline):
-    model = Payment
-    fields=('date', 'amount', 'method')
-    ordering=('date',)
-
-class MemberAdmin(UserAdmin):
-    inlines=[ContactInfoInline, PaymentInfoInline, MembershipPeriodInline, PaymentInline]
-    list_display = ('username', 'email', 'first_name', 'last_name', 'is_staff', 'is_active')
-    list_filter = ('is_staff', 'is_superuser')
-    search_fields = ('username', 'email', 'first_name', 'last_name')
-    ordering = ('username',)
+    def __str__(self):
+        return self.name

cal/models.py
@@ -10,8 +10,8 @@ from django.contrib.sites.models import Site
 from django.db import models
 from django.db.models import permalink, Q
 
-from mos.core.models import Category, Location
-from mos.cal import create_calendar
+from core.models import Category, Location
+from . import create_calendar
 import urllib
 import sys
 

core/models.py
@@ -1,29 +1,29 @@
 from django.db import models
+from django.utils.encoding import python_2_unicode_compatible
 
 
+@python_2_unicode_compatible
 class Category(models.Model):
     """
     represents a Category (name, description)
     used in cal.models.Event
     """
-
     name = models.CharField(max_length=30)
     description = models.CharField(max_length=200)
 
     def __str__(self):
-        return u"%s" % self.name
+        return self.name
 
 
+@python_2_unicode_compatible
 class Location(models.Model):
     """
     represents a location(name, description, country)
     used in cal.models.Event
     """
-
     name = models.CharField(max_length=30)
     description = models.CharField(max_length=200)
-
     country = models.CharField(max_length=100)
 
     def __str__(self):
-        return u"%s" % self.name
+        return self.name

announce/urls.py
@@ -1,5 +1,6 @@
 from django.conf.urls import *
 
+
 urlpatterns = patterns('',
-    (r'^$', 'mos.announce.views.announce'),
+    (r'^$', 'announce.views.announce'),
 )

announce/views.py
@@ -1,15 +1,17 @@
 #
 # Views for issueing announcements to all active members.
 #
+import smtplib
+from datetime import *
+
 from django.shortcuts import render_to_response
 from django.template import RequestContext
 from django.conf import settings
 from django.contrib.auth.decorators import user_passes_test
 import django.forms as forms
-from mos.members.models import get_active_members, ContactInfo
 from django.core.mail import send_mail
-import smtplib
-from datetime import *
+
+from members.models import get_active_members, ContactInfo
 
 
 class AnnouncementForm(forms.Form):

cal/admin.py
@@ -1,6 +1,9 @@
+from __future__ import absolute_import
+
 from django.contrib import admin
 
-from mos.cal.models import Event, Category, Location
+from .models import Event, Category, Location
+
 
 admin.site.register(Event)
 admin.site.register(Category)

cal/forms.py
@@ -1,9 +1,11 @@
+from __future__ import absolute_import
+
 from django.forms import ModelForm
 import django.forms as forms
 
 from django.forms.fields import SplitDateTimeField
 from django.contrib.admin.widgets import AdminSplitDateTime
-from mos.cal.models import Event
+from .models import Event
 
 
 class EventForm(ModelForm):

cal/templatetags/cal_tags.py
@@ -1,7 +1,7 @@
 from django import template
 from django.template import Variable
 
-from mos.cal.models import Event
+from cal.models import Event
 
 
 register = template.Library()

commit abbac1d8bba9ec37b47d03a879622d52dd9cb62e

Fixed regression introduced in 20739f1d.

members/models.py
@@ -8,7 +8,7 @@ from django.db import models
 from django.db.models import Q
 from django.conf import settings
 from django.contrib.auth.models import User
-from django.utils.encoding import python_2_unicode_compatible, smart_unicode
+from django.utils.encoding import python_2_unicode_compatible, smart_unicode, force_str
 
 
 class PaymentInfo(models.Model):
@@ -248,7 +248,7 @@ class PaymentManager(models.Manager):
         import csv
 
         f = open(filename, 'r')
-        r = csv.reader(f, delimiter=";")
+        r = csv.reader(f, delimiter=force_str(';'))
 
         for line in r:
             if len(line) < 2:
@@ -275,7 +275,7 @@ class PaymentManager(models.Manager):
         import csv
 
         f = open(filename, 'r')
-        r = csv.reader(f, delimiter=";")
+        r = csv.reader(f, delimiter=force_str(';'))
 
         for line in r:
             if len(line) < 9:
@@ -302,7 +302,7 @@ class PaymentManager(models.Manager):
         import csv
 
         f = open(filename, 'r')
-        r = csv.reader(f, delimiter=";")
+        r = csv.reader(f, delimiter=force_str(';'))
 
         i = 0
         for line in r:

members/tests/test_import_payments.py
@@ -1,0 +1,32 @@
+import os
+import tempfile
+import shutil
+
+from django.test import TestCase
+from django.core.files.uploadedfile import SimpleUploadedFile
+
+from ..models import Payment
+
+
+CSV = u'01.01.2016;"a line";01.01.2016;123,45;EUR;01.01.2016 01:02:03:456;'
+
+
+class ImportTest(TestCase):
+    def setUp(self):
+        self.tempdir = tempfile.mkdtemp()
+
+    def tearDown(self):
+        shutil.rmtree(self.tempdir)
+
+    def test_delimiter_is_string(self):
+        csv_file = os.path.join(self.tempdir, 'import.csv')
+        with open(csv_file, 'w') as outfile:
+            outfile.write(CSV)
+
+        try:
+            Payment.objects.import_smallfile(csv_file, '2016-01-02')
+        except TypeError as e:
+            if e.message == '"delimiter" must be an 1-character string':
+                assert False, 'The delimiter argument for csv.reader() is of the wrong type.'
+            else:
+                raise