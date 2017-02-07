232bf60a5b341350f44a5422e8b0e9c7de5c1cd0
Christoph Schindler <hop@30hopsmax.at>
2017-02-07 21:42:42 +0100

    Cleaned up auth URLs and linked to password reset form.

--- members/urls.py
+++ members/urls.py
@@ -1,24 +1,23 @@
 from __future__ import absolute_import
 
-from django.conf.urls import *
+from django.conf.urls import url, include
 from django.views.generic.list import ListView
 
 from .models import get_active_members
 
 
-urlpatterns = patterns('',
-    url(r'^login/?$', 'django.contrib.auth.views.login', {'template_name': 'members/member_login.html'}, name='login'),
-    url(r'^logout/?$', 'django.contrib.auth.views.logout', name='logout'),
-    (r'^valid_user/?$', 'members.views.valid_user',),
+urlpatterns = [
+    url(r'^$', ListView.as_view(queryset=get_active_members(), template_name='members/member_list.html')),
 
-    (r'^$', ListView.as_view(queryset=get_active_members(), template_name='members/member_list.html')),
-    (r'^history/$', 'members.views.members_history'),
-    url(r'^change_password/$', 'django.contrib.auth.views.password_change', {'template_name': 'members/member_update_password.html'}, name='password_change'),
-    url(r'^change_password/done/$', 'django.contrib.auth.views.password_change_done', {'template_name': 'members/member_update_password_done.html'}, name='password_change_done'),
-    (r'^collection/$', 'members.views.members_bankcollection_list'),
-    (r'^keylist/$', 'members.views.members_key_list'),
-    (r'^lazzzorlist/$', 'members.views.members_lazzzor_list'),
-    (r'^(?P<user_username>(\w|-)+)/update/userpic/$', 'members.views.members_update_userpic'),
-    (r'^(?P<user_username>(\w|-)+)/update/(?P<update_type>\w+)/$', 'members.views.members_update'),
-    (r'^(?P<user_username>(\w|-)+)/$', 'members.views.members_details'),
-)
+    url('', include('django.contrib.auth.urls')),
+
+    url(r'^valid_user/?$', 'members.views.valid_user',),
+    url(r'^history/$', 'members.views.members_history'),
+    url(r'^collection/$', 'members.views.members_bankcollection_list'),
+    url(r'^keylist/$', 'members.views.members_key_list'),
+    url(r'^lazzzorlist/$', 'members.views.members_lazzzor_list'),
+
+    url(r'^(?P<user_username>(\w|-)+)/update/userpic/$', 'members.views.members_update_userpic'),
+    url(r'^(?P<user_username>(\w|-)+)/update/(?P<update_type>\w+)/$', 'members.views.members_update'),
+    url(r'^(?P<user_username>(\w|-)+)/$', 'members.views.members_details'),
+]
