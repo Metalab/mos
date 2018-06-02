bd3a5ae545226a9514b667766e27a8f3b79edb9c
hettipeti <git@thepeko.at>
2018-06-02 23:44:28 +0200

    removed from this fork the metasense stuff

--- static/javascripts/ml-ajaxtools.js
+++ static/javascripts/ml-ajaxtools.js
@@ -102,7 +102,7 @@ function set_invisible(obj){
 
 function do_on_load()
 {
-    update_metasense();
+    // Do nothing. *Tumbleweed crosses the web*
 }
 
 function enter_pressed(e){
@@ -142,27 +142,4 @@ function submit_event(id) {
         DateTimeShortcuts.init.defer(1);
       }
     })
-}
-
-
-function update_metasense() {
-    /*new Ajax.Request('http://metalab.at/metasense/status.html', {asynchronous:true, onFailure:function(){}, onException:function(){}, onSuccess:function(transport){ */
-    new Ajax.Request('/metasense/status.html', {asynchronous:true, onFailure:function(){}, onException:function(){}, onSuccess:function(transport){
-        oopen = $('presence_open');
-        oclosed = $('presence_closed');
-        odefunct = $('presence_defunct');
-        if(transport.responseText.match("ffnet")) {
-            set_visible(oopen);
-            set_invisible(oclosed);
-            set_invisible(odefunct);
-          } else if( transport.responseText.match("niemand")){
-            set_visible(oclosed);
-            set_invisible(oopen);
-            set_invisible(odefunct);
-        } else {
-            set_invisible(oopen);
-            set_invisible(oclosed);
-            set_visible(odefunct);
-        }
-    }});
 }
