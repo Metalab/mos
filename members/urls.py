c3ba449dcb931617694dc1636e83cef3358acd81
Lothar Komas <thomaskolar90@gmail.com>
2019-10-17 16:34:57 +0200

    allow more special characters in members username URLs, also DRY the code a bit while we're at it

--- members/urls.py
+++ members/urls.py
@@ -5,6 +5,11 @@ from django.views.generic.list import ListView
 
 from .models import get_active_members
 
+username_patterns = [
+    url(r'^update/userpic/$', 'members.views.members_update_userpic'),
+    url(r'^update/(?P<update_type>\w+)/$', 'members.views.members_update'),
+    url(r'^$', 'members.views.members_details'),
+]
 
 urlpatterns = [
     url(r'^$', ListView.as_view(queryset=get_active_members(), template_name='members/member_list.html')),
@@ -17,7 +22,5 @@ urlpatterns = [
     url(r'^keylist/$', 'members.views.members_key_list'),
     url(r'^lazzzorlist/$', 'members.views.members_lazzzor_list'),
 
-    url(r'^(?P<user_username>(\w|-)+)/update/userpic/$', 'members.views.members_update_userpic'),
-    url(r'^(?P<user_username>(\w|-)+)/update/(?P<update_type>\w+)/$', 'members.views.members_update'),
-    url(r'^(?P<user_username>(\w|-)+)/$', 'members.views.members_details'),
+    url(r'^(?P<user_username>([\w\-+.@_])+)/$', include(username_patterns)),
 ]
