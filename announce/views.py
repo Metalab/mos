3059ad7d10772c6a3384dc2d194562920aeef615
Clemens Hopfer <spam-datacop@wireloss.net>
2021-09-14 01:25:42 +0200

    feat: add keymember option to announce, initial variable support

--- announce/views.py
+++ announce/views.py
@@ -18,8 +18,23 @@ class AnnouncementForm(forms.Form):
     body = forms.CharField(required=True, label="Mitteilung",
                            widget=forms.Textarea)
     to = forms.ChoiceField(required=True, label="An",
-                           choices=(('collection', 'collection'), ('all', 'all'),))
+                           choices=(('collection', 'collection'),
+                                    ('keymembers', 'keymembers'),
+                                    ('all', 'all'),))
 
+
+def _announce_filter_collection(users):
+    users = users.filter(paymentinfo__bank_collection_allowed=True) \
+                    .filter(paymentinfo__bank_collection_mode__id=4)
+    for u in users:
+        debt = u.contactinfo.get_debt_for_month(date.today())
+        if debt == 0:
+            users = users.exclude(pk=u.pk)
+    return users
+
+def _announce_filter_keymembers(users):
+    return users.filter(contactinfo__has_active_key=True) \
+                    .exclude(contactinfo__key_id=None)
 
 @staff_member_required
 def announce(request):
@@ -29,19 +44,29 @@ def announce(request):
         return render(request, 'announce/write_message.html', context)
     # Valid message: send it!
     users = get_active_members()
-    if form.cleaned_data['to'] != 'all':
-        users = users.filter(paymentinfo__bank_collection_allowed=True)\
-                     .filter(paymentinfo__bank_collection_mode__id=4)
-        for u in users:
-            debt = u.contactinfo.get_debt_for_month(date.today())
-            if debt == 0:
-                users = users.exclude(pk=u.pk)
+
+    if form.cleaned_data['to'] == 'collection':
+        users = _announce_filter_collection(users)
+    elif form.cleaned_data['to'] == 'keymembers':
+        users = _announce_filter_keymembers(users)
 
     for user in users:
+        body = form.cleaned_data['body'] \
+            .replace('{{username}}', user.get_username()) \
+            .replace('{{full_name}}', user.get_full_name()) \
+            .replace('{{short_name}}', user.get_short_name()) \
+            .replace('{{first_name}}', user.first_name) \
+            .replace('{{last_name}}', user.last_name) \
+            .replace('{{user_id}}', str(user.pk)) \
+            .replace('{{profile_link}}',
+                f'https://{settings.SESSION_COOKIE_DOMAIN}/member/{user.get_username()}/') \
+            .replace('{{IBAN}}', str(settings.HOS_SEPA_CREDITOR_IBAN)) \
+            .replace('{{BIC}}', str(settings.HOS_SEPA_CREDITOR_BIC))
+
         ci = ContactInfo.objects.get(user=user)
         try:
             send_mail(form.cleaned_data['subject'],
-                      form.cleaned_data['body'],
+                      body,
                       settings.HOS_ANNOUNCE_FROM,
                       [user.email],
                       fail_silently=False)
