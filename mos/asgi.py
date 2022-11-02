26901dc576d393a462451252aac45e0440634886
Hetti <git@thepeko.at>
2022-11-03 00:03:11 +0100

    fix: asgi.py for daphne

--- mos/asgi.py
+++ mos/asgi.py
@@ -4,10 +4,25 @@ defined in the ASGI_APPLICATION setting.
 """
 
 import os
-
 import django
+
 from channels.routing import get_default_application
+from channels.routing import ProtocolTypeRouter, URLRouter
+from channels.auth import AuthMiddlewareStack
+from django.core.asgi import get_asgi_application
+from django.urls import path
+from django.conf.urls import url
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "settings.devel")
 django.setup()
-application = get_default_application()
+
+django_asgi_app = get_asgi_application()
+
+application = ProtocolTypeRouter({
+    # Django's ASGI application to handle traditional HTTP requests
+    "http": django_asgi_app,
+    "websocket": AuthMiddlewareStack(
+        URLRouter([
+        ])
+    ),
+})
