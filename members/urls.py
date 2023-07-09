21d0d37c0515b5274307fba071dabe5fed085f43
luto <m@luto.at>
2023-07-09 19:40:39 +0200

    remove unused payment code

--- members/urls.py
+++ members/urls.py
@@ -23,7 +23,6 @@ urlpatterns = [
     re_path(r'^valid_user/?$', members.views.valid_user),
     path('history/', members.views.members_history),
     path('hetti/', members.views.hetti),
-    path('collection/', members.views.members_bankcollection_list),
     path('bank/', members.views.members_bank),
     path('bank/json/import', members.views.members_bank_json_import),
     path('bank/json/match', members.views.members_bank_json_match),
