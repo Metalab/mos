aa5fb67ec83b7292cecf7e18843b31754667af3d
qch3n <tiefengraber.bruno@gmail.com>
2024-02-24 23:28:36 +0100

    only check wikiPage special requirements if it is set

--- cal/forms.py
+++ cal/forms.py
@@ -29,13 +29,15 @@ class EventForm(ModelForm):
         end_date = cleaned_data.get('endDate')
         if end_date and end_date < start_date:
             self.add_error('endDate', 'End date must be greater than start date')
-        wikipage, _ = re.subn(r'(^http(s)://metalab.at/wiki/|\ |\%|\&)', '', cleaned_data.get('wikiPage'), 200)
-        cleaned_data['wikiPage'] = wikipage
-        if cleaned_data.get('advertise') and re.match(r'^(Benutzer|User):', wikipage):
-            self.add_error('wikiPage', 'Userpages don\'t provide adequate information for public Events')
 
-        r = requests.get('https://metalab.at/wiki/%s' % wikipage)
+        if cleaned_data.get('wikiPage'):
+            wikipage, _ = re.subn(r'(^http(s)://metalab.at/wiki/|\.\.|\ |\%|\&)', '', cleaned_data.get('wikiPage'), 200)
+            cleaned_data['wikiPage'] = wikipage
+            if cleaned_data.get('advertise') and re.match(r'^(Benutzer|User):', wikipage):
+                self.add_error('wikiPage', 'Userpages don\'t provide adequate information for public Events')
 
-        if r.status_code == 404:
-            self.add_error('wikiPage', 'Wikipage not found: https://metalab.at/wiki/%s' % wikipage) #TODO Figure out how to make clickable
+            r = requests.get('https://metalab.at/wiki/%s' % wikipage)
+            
+            if r.status_code == 404:
+                self.add_error('wikiPage', 'Wikipage not found: https://metalab.at/wiki/%s' % wikipage) #TODO Figure out how to make clickable
         return cleaned_data
