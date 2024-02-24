2c63fc2bf8f668955a0f40e0d6fac04a08c04647
qch3n <tiefengraber.bruno@gmail.com>
2024-02-24 20:36:18 +0100

    check if wikipage exists and disallow user pages for public events

--- cal/forms.py
+++ cal/forms.py
@@ -4,7 +4,8 @@ import django.forms as forms
 from django.forms.fields import SplitDateTimeField
 from django.contrib.admin.widgets import AdminSplitDateTime
 from .models import Event
-import re  
+import re
+import requests
 
 
 class EventForm(ModelForm):
@@ -27,5 +28,13 @@ class EventForm(ModelForm):
         end_date = cleaned_data.get('endDate')
         if end_date and end_date < start_date:
             self.add_error('endDate', 'End date must be greater than start date')
-        cleaned_data['wikiPage'] = re.sub(r'^http(s)://metalab.at/wiki/', '',cleaned_data.get('wikiPage'))
+        wikipage = re.sub(r'^http(s)://metalab.at/wiki/', '',cleaned_data.get('wikiPage'))
+        cleaned_data['wikiPage'] = wikipage
+        if cleaned_data.get('advertise') and re.match(r'^(Benutzer|User):', wikipage):
+            self.add_error('wikiPage', 'Userpages don\'t provide adequate information for public Events')
+
+        r = requests.get('https://metalab.at/wiki/%s' % wikipage)
+        
+        if r.status_code == 404:
+            self.add_error('wikiPage', 'Wikipage not found: https://metalab.at/wiki/%s' % wikipage) #TODO Figure out how to make clickable
         return cleaned_data
